2ad47375a7d5caecf36bfba5d415313a9a2f9e7a
Rushibi <rushibi1996@gmail.com>
2024-10-13 12:59:49 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿namespace SingleTonDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            SingleTon s1 = SingleTon.getSingleTonInstance();
+
+            SingleTon s2 = SingleTon.getSingleTonInstance();
+
+            if (s1 == s2)
+            {
+                Console.WriteLine("singleton works");
+                s1.someLogic().StringSet("Key", "Value");
+
+                Console.WriteLine(s2.someLogic().StringGet("key"));
+            }
+            else
+            {
+
+                Console.WriteLine("singleton not works");
+            }
+            Console.ReadLine();
+        }
+    }
+}
